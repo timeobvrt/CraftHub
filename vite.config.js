748c63b66c07231998e35b793d4d5b5623cf1bbd
Timéo Bavart <timeo.bavart@epitech.eu>
2026-09-22 16:37:02 +0200

    fix: add css to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/css/app.css", "resources/js/app.js", "resources/css/all.css", 'resources/css/pixel-regular.css'],
             refresh: true,
             fonts: [
                 bunny("Instrument Sans", {
